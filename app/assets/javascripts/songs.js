681a83e756fd2689cefe45312a3638ec71103fa3
ajk <ajk@f2badfa2-83dd-4ef1-9e2d-de321b006519>
2012-02-19 21:59:47 +0000

    yay
    git-svn-id: https://ajk.tontut.fi/svn/beat-stream/src/trunk@4 f2badfa2-83dd-4ef1-9e2d-de321b006519

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,176 +1,226 @@
-(function ($) {
-	$(document).ready(function () {
-		var songs = $('#songlist li');
-		var audio = $('#player audio');
-		audio.css('display', 'none');
+//= require slick.grid
 
-		var playerTrack = $('#player-song .track');
-		var playPause = $('#play-pause');
-		var prevButton = $('#prev');
-		var nextButton = $('#next');
-		var elapsed = $('#player-time .elapsed');
-		var duration = $('#player-time .duration');
-		var shuffleButton = $('#shuffle');
+$(document).ready(function () {
+	var songs = $('#songlist li');
+	var audio = $('#player audio');
+	audio.css('display', 'none');
 
-		var currentSong = null;
-		var shuffle = false;
+	var playerTrack = $('#player-song .track');
+	var playPause = $('#play-pause');
+	var prevButton = $('#prev');
+	var nextButton = $('#next');
+	var elapsed = $('#player-time .elapsed');
+	var duration = $('#player-time .duration');
+	var shuffleButton = $('#shuffle');
 
-		if (store.get('shuffle')) {
-			shuffle = store.get('shuffle');
+	var currentSong = null;
+	var shuffle = false;
+
+	if (store.get('shuffle')) {
+		shuffle = store.get('shuffle');
+	}
+
+	// volume slider
+	var volume = 0.6;
+	if (store.get('volume')) {
+		volume = parseFloat(store.get('volume'));
+		if (volume >= 0 && volume <= 1.0) {
+			audio[0].volume = volume;
 		}
+	}
 
-		// volume slider
-		var volume = 0.6;
-		if (store.get('volume')) {
-			volume = parseFloat(store.get('volume'));
-			if (volume >= 0 && volume <= 1.0) {
-				audio[0].volume = volume;
-			}
+	$('#player-volume-slider').slider({
+		orientation: 'horizontal',
+		value: volume * 100,
+		max: 100,
+		min: 0,
+		range: 'min',
+		slide: function (event, ui) {
+			audio[0].volume = parseFloat(ui.value/100);
+		},
+		stop: function (event, ui) {
+			store.set('volume', parseFloat(ui.value/100));
 		}
+	});
 
-		$('#player-volume-slider').slider({
-			orientation: 'horizontal',
-			value: volume * 100,
-			max: 100,
-			min: 0,
-			range: 'min',
-			slide: function (event, ui) {
-				audio[0].volume = parseFloat(ui.value/100);
-			},
-			stop: function (event, ui) {
-				store.set('volume', parseFloat(ui.value/100));
-			}
-		});
-
-		// seekbar
-		var seekbar = $('#seekbar-slider');
-		var user_is_seeking = false;
-		seekbar.slider({
-			orientation: 'horizontal',
-			disabled: true,
-			value: 0,
-			max: 100,
-			min: 0,
-			range: 'min',
-			slide: function (event, ui) {
-				console.log(ui.value);
-			},
-			start: function(event, ui) {
-				user_is_seeking = true;
-			},
-			stop: function(event, ui) {
-				audio[0].currentTime = ui.value;
-				user_is_seeking = false;
-			}
-		});
-
-		playPause.click(function (e) {
-			if (audio[0].paused) {
-				audio[0].play();
-			}
-			else {
-				audio[0].pause();
-			}
-			e.preventDefault();
-		});
-
-		shuffleButton.click(function (e) {
-			shuffle = !shuffle;
-			store.set('shuffle', shuffle);
-			if (shuffle) {
-				alert('shuffle is ENABLED!');
-			}
-			else {
-				alert('shuffle is DISABLED');
-			}
-		});
-
-		audio.bind('play', function() {
-			playPause.removeClass('paused');
-			$('#player-buttons button').removeAttr('disabled');
-		});
-
-		audio.bind('pause', function() {
-			playPause.addClass('paused');
-		});
-
-		audio.bind('ended', function () {
-			//seekbar.slider('option', 'disabled', true);
-			nextSong();
-		});
-
-		audio.bind('timeupdate', function () {
-			var elaps = parseInt(audio[0].currentTime),
-				mins = Math.floor(elaps/60, 10),
-				secs = elaps - mins*60;
-
-			elapsed.text((mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs));
-
-			if (!user_is_seeking) {
-				seekbar.slider('option', 'value', elaps);
-			}
-		});
-
-		audio.bind('durationchange', function () {
-			var dur = parseInt(audio[0].duration),
-				mins = Math.floor(dur/60, 10),
-				secs = dur - mins*60;
-
-			duration.text((mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs));
-
-			seekbar.slider('option', 'max', dur);
-			seekbar.slider('option', 'disabled', false);
-		});
-
-		function prevSong() {
-			currentSong.prev('li').click();
+	// seekbar
+	var seekbar = $('#seekbar-slider');
+	var user_is_seeking = false;
+	seekbar.slider({
+		orientation: 'horizontal',
+		disabled: true,
+		value: 0,
+		max: 100,
+		min: 0,
+		range: 'min',
+		slide: function (event, ui) {
+			console.log(ui.value);
+		},
+		start: function(event, ui) {
+			user_is_seeking = true;
+		},
+		stop: function(event, ui) {
+			audio[0].currentTime = ui.value;
+			console.log(ui.value);
+			user_is_seeking = false;
 		}
+	});
 
-		function nextSong() {
-			if (shuffle) {
-				var numbah = randomToN(songs.length);
-				$(songs.get(numbah)).click();
-			}
-			else {
-				currentSong.next('li').click();
-			}
-		}
-
-		// play songs when the name is clicked!
-		songs.click(function (e) {
-			e.preventDefault();
-
-			var $this = $(this),
-				filename = $.trim($this.text()),
-				uri = $this.find('a').attr('href');
-
-			playerTrack.text(filename);
-
-			audio.attr('src', uri);
+	playPause.click(function (e) {
+		e.preventDefault();
+		if (audio[0].paused) {
 			audio[0].play();
+		}
+		else {
+			audio[0].pause();
+		}
+		e.preventDefault();
+	});
 
-			if (currentSong) currentSong.removeClass('now-playing');
-			currentSong = $this;
-			currentSong.addClass('now-playing');
-		});
+	shuffleButton.click(function (e) {
+		e.preventDefault();
+		shuffle = !shuffle;
+		store.set('shuffle', shuffle);
+		if (shuffle) {
+			alert('shuffle is ENABLED!');
+		}
+		else {
+			alert('shuffle is DISABLED');
+		}
+	});
 
+	nextButton.click(function (e) {
+		e.preventDefault();
+		nextSong();
+	});
+
+	prevButton.click(function (e) {
+		e.preventDefault();
+		prevSong();
+	});
+
+	audio.bind('play', function() {
+		playPause.removeClass('paused');
+		$('#player-buttons button').removeAttr('disabled');
+	});
+
+	audio.bind('pause', function() {
+		playPause.addClass('paused');
+	});
+
+	audio.bind('ended', function () {
+		//seekbar.slider('option', 'disabled', true);
+		nextSong();
+	});
+
+	audio.bind('timeupdate', function () {
+		var elaps = parseInt(audio[0].currentTime),
+			mins = Math.floor(elaps/60, 10),
+			secs = elaps - mins*60;
+
+		elapsed.text((mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs));
+
+		if (!user_is_seeking) {
+			seekbar.slider('option', 'value', elaps);
+		}
+	});
+
+	audio.bind('durationchange', function () {
+		var dur = parseInt(audio[0].duration),
+			mins = Math.floor(dur/60, 10),
+			secs = dur - mins*60;
+
+		duration.text((mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs));
+
+		seekbar.slider('option', 'max', dur);
+		seekbar.slider('option', 'disabled', false);
+	});
+
+	function prevSong() {
+		playSong(currentSong.prev('li'));
+	}
+
+	function nextSong() {
+		if (shuffle) {
+			var numbah = randomToN(songs.length);
+			playSong($(songs.get(numbah)));
+		}
+		else {
+			playSong(currentSong.next('li'));
+		}
+	}
+
+	function playSong(song) {
+		var $this = song,
+			filename = $.trim($this.text()),
+			uri = $this.find('a').attr('href');
+
+		playerTrack.text(filename);
+
+		audio.attr('src', uri);
+		audio[0].play();
+
+		if (currentSong) currentSong.removeClass('now-playing');
+		currentSong = $this;
+		currentSong.addClass('now-playing');
+	}
+
+	// play songs when the name is clicked!
+	songs.click(function (e) {
+		e.preventDefault();
+		playSong($(this));
 	});
 
 	// resize the main-area to correct height
-	$(document).ready(function () {
-		function resizeMain() {
-			var h = $(window).height() - $('#wrap > header').innerHeight() - $('#player').innerHeight();
-			$('#main').css('height', h);
-		}
+	function resizeMain() {
+		var h = $(window).height() - $('#wrap > header').innerHeight() - $('#player').innerHeight();
+		var w = $(window).width() - $('#sidebar').innerWidth();
+		$('#main').css('height', h);
+		$('#content-wrap').css('width', w);
+	}
 
+	$(window).resize(function () {
 		resizeMain();
+	});
+	$(window).resize();
 
-		$(window).resize(function () {
-			resizeMain();
-		});
+
+	// SlickGrid
+
+	var columns = [
+		{ id: 'artist', name: 'Artist', field: 'artist' },
+		{ id: 'tracknum', name: '#', field: 'tracknum', cssClass: 'tracknum', width: 20 },
+		{ id: 'title', name: 'Title', field: 'title' },
+		{ id: 'album', name: 'Album', field: 'album' },
+		{ id: 'duration', name: 'Duration', field: 'nice_length', cssClass: 'duration', width: 30 },
+		{ id: 'path', name: '', field: 'path' }
+	];
+
+	var options = {
+		autoHeight: true,
+		forceFitColumns: true,
+		enableCellNavigation: true,
+		enableColumnReorder: false
+	};
+
+	var grid = null;
+
+  	$.getJSON('/songs/index', function(data) {
+  		return;
+    	grid = new Slick.Grid("#slickgrid", data, columns, options);
+
+    	// double-click => play song
+    	grid.onDblClick.subscribe(function (e) {
+    		var cell = grid.getCellFromEvent(e);
+
+    		playSong(data[cell.row].path);
+    		e.stopPropagation();
+    	});
+
+		console.log(grid);
 	});
 
-})(jQuery);
+});
 
 //function to get random number from 1 to n
 function randomToN(maxVal,floatVal)
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,53 +1,71 @@
 require 'find'
 
-#MUSIC_PATH = '/Users/ajk/Music/'
-MUSIC_PATH = '/Volumes/work/Musa/'
+MUSIC_PATH = '/Users/ajk/Music/'
+#MUSIC_PATH = '/Volumes/work/Musa/'
 
 class SongsController < ApplicationController
 
   	def index
   		@songs = Rails.cache.fetch('songs')
   		if @songs.nil? || @songs.empty? || params['refresh']
-  			@songs = []
-  			Find.find(MUSIC_PATH) do |file|
-	  			next if file !~ /.*mp3$/
-  				#mp3info = Mp3Info.open(file)
-  				@songs << file.gsub(MUSIC_PATH, '') #+ "(" + mp3info.length.to_s + ")"
-  			end
-  			@songs.sort!
-  			Rails.cache.write('songs', @songs, :time_to_idle => 1.minute, :timeToLive => 10.minutes)
+  			refresh
   		end
+
+  		respond_to do |format|
+  			format.html
+  			format.json { render :json => @songs }
+  		end
+  	end
+
+  	def refresh
+		@songs = []
+		Find.find(MUSIC_PATH) do |file|
+ 			next if file !~ /.*mp3$/
+ 			mp3 = Mp3File.new(file)
+ 			@songs.push(mp3)
+		end
+ 		Rails.cache.write('songs', @songs, :time_to_idle => 1.minute, :timeToLive => 1440.minutes)
   	end
 
   	def play
   		filename = params[:file]
   		filepath = MUSIC_PATH + filename
 
-		send_file filepath, :type => 'audio/mpeg'
+		response.content_type = Mime::Type.lookup_by_extension("mp3")
+
+  		render :text => File.open(filepath, 'rb') { |f| f.read }
+		#send_file filepath, :type => 'audio/mpeg'
   	end
 end
 
 class Mp3File
 
-    attr_reader :artist, :album, :title, :file, :path, :length, :type, :mtime
-    attr_writer :artist, :album, :title, :file, :path, :length, :type, :mtime
+    attr_reader :file, :filename, :path, :artist, :title, :album, :tracknum, :length, :type, :mtime
 
     def initialize(path)
       	file = File.new(path)
       	info = Mp3Info.open(path)
       	tag = info.tag()
+      	
       	@file = path
+      	@filename = File.basename(path)
       	@artist = tag['artist']
-      	@album = tag['album']
       	@title = tag['title']
-	    @path = file.path()
-      	@length = file.stat.size()
-      	@mtime = file.stat.mtime()
+      	@album = tag['album']
+      	@tracknum = tag['tracknum']
+      	@length = info.length
+
+	    @path = file.path().gsub(MUSIC_PATH, '')
+      	@size = file.stat.size()
       	@type = 'audio/mpeg'
+      	@mtime = file.stat.mtime()
+
+      	@nice_title = (@artist || @title ? @artist.to_s + " - " + @title.to_s : @filename)
+      	@nice_length = (Time.mktime(0)+@length).strftime("%M:%S")
     end
 
     def to_s
-      	@title
+      	@nice_title
     end
 
 end
