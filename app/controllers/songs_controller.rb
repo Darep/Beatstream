ec7ed8ec72b9250c05e7f608b7f10c7276c48584
ajk <ajk@f2badfa2-83dd-4ef1-9e2d-de321b006519>
2012-02-20 22:37:36 +0000

    Add ISO-8859-15 -> UTF-8 conversion to ID3 tag info
    git-svn-id: https://ajk.tontut.fi/svn/beat-stream/src/trunk@11 f2badfa2-83dd-4ef1-9e2d-de321b006519

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,42 +1,43 @@
+# -*- encoding : utf-8 -*-
 require 'find'
 
 MUSIC_PATH = Rails.application.config.MUSIC_PATH
 
 class SongsController < ApplicationController
 
-  	def index
-  		@songs = Rails.cache.fetch('songs')
-  		if @songs.nil? || @songs.empty? || params['refresh']
-  			refresh
-  		end
+    def index
+        @songs = Rails.cache.fetch('songs')
+        if @songs.nil? || @songs.empty? || params['refresh'] || true
+            refresh
+        end
 
-  		respond_to do |format|
-  			format.json { render :json => @songs }
-  		end
-  	end
+        respond_to do |format|
+            format.json { render :json => @songs }
+        end
+    end
 
-  	def play
-  		filename = params[:file]
-  		filepath = MUSIC_PATH + filename
+    def play
+        filename = params[:file]
+        filepath = MUSIC_PATH + filename
 
-		response.content_type = Mime::Type.lookup_by_extension("mp3")
+        response.content_type = Mime::Type.lookup_by_extension("mp3")
 
-  		render :text => File.open(filepath, 'rb') { |f| f.read }
-		#send_file filepath, :type => 'audio/mpeg'
-  	end
+        render :text => File.open(filepath, 'rb') { |f| f.read }
+        #send_file filepath, :type => 'audio/mpeg'
+    end
 
-	private
+    private
 
-  	def refresh
-		@songs = []
-		Find.find(MUSIC_PATH) do |file|
- 			next if file !~ /.*\.mp3$/
- 			next if File.directory?(file)
- 			mp3 = Mp3File.new(file)
- 			@songs.push(mp3)
-		end
- 		Rails.cache.write('songs', @songs, :time_to_idle => 1.minute, :timeToLive => 1440.minutes)
-  	end
+    def refresh
+        @songs = []
+        Find.find(MUSIC_PATH) do |file|
+            next if file !~ /.*\.mp3$/
+            next if File.directory?(file)
+            mp3 = Mp3File.new(file)
+            @songs.push(mp3)
+        end
+        Rails.cache.write('songs', @songs, :time_to_idle => 1.minute, :timeToLive => 1.day)
+    end
 end
 
 class Mp3File
@@ -44,42 +45,58 @@ class Mp3File
     attr_reader :filename, :path, :artist, :title, :album, :tracknum, :length
 
     def initialize(path)
-      	file = File.new(path)
+        file = File.new(path)
 
-      	@filename = File.basename(path)
-	    @path = path.gsub(MUSIC_PATH, '')
-      	#@size = file.stat.size()
+        @filename = File.basename(path)
+        @path = path.gsub(MUSIC_PATH, '')
+        #@size = file.stat.size()
 
-      	@title = @filename
-		@artist = ''
-		@album = ''
-		@tracknum = ''
-		@length = 0
+        @title = @filename
+        @artist = ''
+        @album = ''
+        @tracknum = ''
+        @length = 0
 
-      	# ID3 tag info
-		begin
-      		info = Mp3Info.open(path)
-	      	tag = info.tag()
-	      	@title = tag['title'] if (!tag['title'].nil?)
-	      	@artist = tag['artist'] if (!tag['title'].nil?)
-	      	@album = tag['album']
-	      	@tracknum = tag['tracknum']
-	      	@length = info.length
-		rescue Mp3InfoError
-			# don't take broken mp3s
-			# TODO: collect the broken mp3s into a separate array
-			# TODO: tell the caller how many broken mp3s found
-		end
-      	
+        # ID3 tag info
+        begin
+            info = Mp3Info.open(path)
+            tag = info.tag()
+            @title = tag['title'] if (!tag['title'].nil?)
+            @artist = tag['artist'] if (!tag['title'].nil?)
+            @album = tag['album']
+            @tracknum = tag['tracknum']
+            @length = info.length
+        rescue Mp3InfoError
+            # TODO: collect the broken mp3s into a separate array
+            # TODO: count the broken mp3s
+        end
+        
         @nice_title = ''
-      	@nice_title += (@artist.to_s + ' - ') if (!@artist.nil?)
-      	@nice_title += @title.to_s
+        @nice_title += (@artist.to_s + ' - ') if !@artist.nil?
+        @nice_title += @title.to_s
 
-      	@nice_length = (Time.mktime(0)+@length).strftime("%m:%S")
+        @nice_length = (Time.mktime(0)+@length).strftime("%m:%S")
+
+        # convert outgoing strings into valid utf-8
+        @title = to_utf8(@title)
+        @artist = to_utf8(@artist) if !@artist.nil?
+        @album = to_utf8(@album) if !@album.nil?
+        @nice_title = to_utf8(@nice_title)
     end
 
     def to_s
-      	@nice_title
+        @nice_title
     end
 
+
+    # Iconv UTF-8 helper
+    # Converts string into valid UTF-8
+    #
+    # @param [String] untristed_string the string to convert to UTF-8
+    # @return [String] passed string in UTF-8
+    def to_utf8 untrusted_string=""
+        ic = Iconv.new('UTF-8//IGNORE', 'ISO-8859-15')
+        ic.iconv(untrusted_string)
+        #ic.iconv(untrusted_string + ' ')[0..-2]
+    end
 end
