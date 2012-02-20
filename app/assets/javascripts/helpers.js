e785750635d4672fde2703ec583de60bc4f65ba3
ajk <ajk@f2badfa2-83dd-4ef1-9e2d-de321b006519>
2012-02-20 23:23:02 +0000

    Refactoring
    git-svn-id: https://ajk.tontut.fi/svn/beat-stream/src/trunk@14 f2badfa2-83dd-4ef1-9e2d-de321b006519

---
+++ app/assets/javascripts/helpers.js
@@ -0,0 +1,12 @@
+/*!
+ * Helper functions
+ *
+ */
+
+
+// function to get random number from 1 to n
+function randomToN(maxVal,floatVal)
+{
+   var randVal = Math.random()*maxVal;
+   return typeof floatVal=='undefined'?Math.round(randVal):randVal.toFixed(floatVal);
+}
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,273 +1,272 @@
 //= require store.min
 //= require slick.grid
+//= require helpers
 
 $(document).ready(function () {
-	var songs = null;
-	var grid = null;
-	var audio = $('#player audio');
-	audio.css('display', 'none');
 
-	var playerTrack = $('#player-song .track');
-	var playPause = $('#play-pause');
-	var prevButton = $('#prev');
-	var nextButton = $('#next');
-	var elapsed = $('#player-time .elapsed');
-	var duration = $('#player-time .duration');
+    // resize the main-area to correct height
+    function resizeMain() {
+        var h = $(window).height() - $('#wrap > header').innerHeight() - $('#player').innerHeight();
+        var w = $(window).width() - $('#sidebar').innerWidth();
+        $('#main').css('height', h);
+        $('#content-wrap').css('width', w);
 
-	var currentSong = null;
+        if (grid)
+            grid.resizeCanvas();
+    }
 
-	// shuffle
-	var shuffleButton = $('#shuffle');
-	var shuffle = false;
+    $(window).resize(function () {
+        resizeMain();
+    });
+    $(window).resize();
 
-	if (store.get('shuffle')) {
-		shuffle = store.get('shuffle');
-	}
+    
+    // init:
 
-	if (shuffle) {
-		shuffleButton.addClass('enabled');
-	}
+    var songs = null;
+    var grid = null;
+    var audio = $('#player audio');
+    audio.css('display', 'none');
 
-	shuffleButton.click(function (e) {
-		e.preventDefault();
+    var playerTrack = $('#player-song .track');
+    var playPause = $('#play-pause');
+    var prevButton = $('#prev');
+    var nextButton = $('#next');
+    var elapsed = $('#player-time .elapsed');
+    var duration = $('#player-time .duration');
 
-		shuffle = !shuffle;
-		store.set('shuffle', shuffle);
+    var currentSong = null;
 
-		$(this).toggleClass('enabled');
-	});
+    // shuffle
+    var shuffleButton = $('#shuffle');
+    var shuffle = false;
 
-	// volume slider
-	var volume = 0.6;
-	if (store.get('volume')) {
-		volume = parseFloat(store.get('volume'));
-		if (volume >= 0 && volume <= 1.0) {
-			audio[0].volume = volume;
-		}
-	}
+    if (store.get('shuffle')) {
+        shuffle = store.get('shuffle');
+    }
 
-	$('#player-volume-slider').slider({
-		orientation: 'horizontal',
-		value: volume * 100,
-		max: 100,
-		min: 0,
-		range: 'min',
-		slide: function (event, ui) {
-			audio[0].volume = parseFloat(ui.value/100);
-		},
-		stop: function (event, ui) {
-			store.set('volume', parseFloat(ui.value/100));
-		}
-	});
+    if (shuffle) {
+        shuffleButton.addClass('enabled');
+    }
 
-	// seekbar
-	var seekbar = $('#seekbar-slider');
-	var user_is_seeking = false;
-	seekbar.slider({
-		orientation: 'horizontal',
-		disabled: true,
-		value: 0,
-		max: 100,
-		min: 0,
-		range: 'min',
-		slide: function (event, ui) {
-		},
-		start: function(event, ui) {
-			user_is_seeking = true;
-		},
-		stop: function(event, ui) {
-			audio[0].currentTime = ui.value;
-			user_is_seeking = false;
-		}
-	});
+    shuffleButton.click(function (e) {
+        e.preventDefault();
 
-	playPause.click(function (e) {
-		e.preventDefault();
-		if (audio[0].paused) {
-			audio[0].play();
-		}
-		else {
-			audio[0].pause();
-		}
-		e.preventDefault();
-	});
+        shuffle = !shuffle;
+        store.set('shuffle', shuffle);
 
-	nextButton.click(function (e) {
-		e.preventDefault();
-		nextSong();
-	});
+        $(this).toggleClass('enabled');
+    });
 
-	prevButton.click(function (e) {
-		e.preventDefault();
-		prevSong();
-	});
+    // volume slider
+    var volume = 0.6;
+    if (store.get('volume')) {
+        volume = parseFloat(store.get('volume'));
+        if (volume >= 0 && volume <= 1.0) {
+            audio[0].volume = volume;
+        }
+    }
 
-	audio.bind('play', function() {
-		playPause.removeClass('paused');
-		$('#player-buttons button').removeAttr('disabled');
-	});
+    $('#player-volume-slider').slider({
+        orientation: 'horizontal',
+        value: volume * 100,
+        max: 100,
+        min: 0,
+        range: 'min',
+        slide: function (event, ui) {
+            audio[0].volume = parseFloat(ui.value/100);
+        },
+        stop: function (event, ui) {
+            store.set('volume', parseFloat(ui.value/100));
+        }
+    });
 
-	audio.bind('pause', function() {
-		playPause.addClass('paused');
-	});
+    // seekbar
+    var seekbar = $('#seekbar-slider');
+    var user_is_seeking = false;
+    seekbar.slider({
+        orientation: 'horizontal',
+        disabled: true,
+        value: 0,
+        max: 100,
+        min: 0,
+        range: 'min',
+        slide: function (event, ui) {
+        },
+        start: function(event, ui) {
+            user_is_seeking = true;
+        },
+        stop: function(event, ui) {
+            audio[0].currentTime = ui.value;
+            user_is_seeking = false;
+        }
+    });
 
-	audio.bind('ended', function () {
-		//seekbar.slider('option', 'disabled', true);
-		nextSong();
-	});
+    playPause.click(function (e) {
+        e.preventDefault();
+        if (audio[0].paused) {
+            audio[0].play();
+        }
+        else {
+            audio[0].pause();
+        }
+        e.preventDefault();
+    });
 
-	audio.bind('timeupdate', function () {
-		var elaps = parseInt(audio[0].currentTime),
-			mins = Math.floor(elaps/60, 10),
-			secs = elaps - mins*60;
+    nextButton.click(function (e) {
+        e.preventDefault();
+        nextSong();
+    });
 
-		elapsed.text((mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs));
+    prevButton.click(function (e) {
+        e.preventDefault();
+        prevSong();
+    });
 
-		if (!user_is_seeking) {
-			seekbar.slider('option', 'value', elaps);
-		}
-	});
+    audio.bind('play', function() {
+        playPause.removeClass('paused');
+        $('#player-buttons button').removeAttr('disabled');
+    });
 
-	audio.bind('durationchange', function () {
-		var dur = parseInt(audio[0].duration),
-			mins = Math.floor(dur/60, 10),
-			secs = dur - mins*60;
+    audio.bind('pause', function() {
+        playPause.addClass('paused');
+    });
 
-		duration.text((mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs));
+    audio.bind('ended', function () {
+        //seekbar.slider('option', 'disabled', true);
+        nextSong();
+    });
 
-		seekbar.slider('option', 'max', dur);
-		seekbar.slider('option', 'disabled', false);
-	});
+    audio.bind('timeupdate', function () {
+        var elaps = parseInt(audio[0].currentTime),
+            mins = Math.floor(elaps/60, 10),
+            secs = elaps - mins*60;
 
+        elapsed.text((mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs));
 
-	// SlickGrid
+        if (!user_is_seeking) {
+            seekbar.slider('option', 'value', elaps);
+        }
+    });
 
-	var columns = [
-		{ id: 'np', resizable: false, width: 20 },
-		{ id: 'title', name: 'Title', field: 'title', sortable: true },
-		{ id: 'tracknum', name: '', field: 'tracknum', sortable: true, resizable: false, cssClass: 'tracknum', width: 30 },
-		{ id: 'artist', name: 'Artist', field: 'artist', sortable: true },
-		{ id: 'album', name: 'Album', field: 'album', sortable: true },
-		{ id: 'duration', name: 'Duration', field: 'nice_length', sortable: true, cssClass: 'duration', width: 30 },
-		{ id: 'path', name: '', field: 'path' }
-	];
+    audio.bind('durationchange', function () {
+        var dur = parseInt(audio[0].duration),
+            mins = Math.floor(dur/60, 10),
+            secs = dur - mins*60;
 
-	var options = {
-		autoHeight: true,
-		forceFitColumns: true,
-		enableCellNavigation: false,
-		enableColumnReorder: false,
-		multiSelect: false,
-		rowHeight: 22
-	};
+        duration.text((mins > 9 ? mins : '0' + mins) + ':' + (secs > 9 ? secs : '0' + secs));
 
-	$.getJSON('/songs/index', function(data) {
-		grid = new Slick.Grid("#slickgrid", data, columns, options);
-		grid.setSelectionModel(new Slick.RowSelectionModel());
-
-		// remove 'path' column
-		grid.setColumns(columns.slice(0, -1));
-
-		console.log(grid);
-
-    	// double-click => play song
-    	grid.onClick.subscribe(function (e) {
-    		var cell = grid.getCellFromEvent(e);
-
-    		grid.setSelectedRows([cell.row]);
-    	});
-
-    	grid.onDblClick.subscribe(function (e) {
-    		var cell = grid.getCellFromEvent(e);
-
-    		grid.startPlaying(cell.row);
-
-    		e.stopPropagation();
-    	});
-
-    	grid.onSelectedRowsChanged.subscribe(function (e) {
-    		var row = grid.getSelectedRows()[0];
-    	});
-
-    	grid.currentSong = null;
-
-    	grid.startPlaying = function (row) {
-    		var song = grid.getDataItem(row);
-    		
-    		playSong(song.nice_title, song.path);
-
-    		// now playing icon
-    		var cells = {};
-    		cells[row] = { np: 'playing' };
-
-    		grid.removeCellCssStyles('currentSong_playing');
-    		grid.addCellCssStyles('currentSong_playing', cells);
-
-    		grid.currentSong = row;
-    		grid.setSelectedRows([row]);
-    	};
-	});
+        seekbar.slider('option', 'max', dur);
+        seekbar.slider('option', 'disabled', false);
+    });
 
 
-	// song playback functions
+    // SlickGrid
 
-	function prevSong() {
-		var newRow = grid.getDataLength() - 1;
-		var currentRow = grid.currentSong; //grid.getSelectedRows()[0];
+    var columns = [
+        { id: 'np', resizable: false, width: 20 },
+        { id: 'title', name: 'Title', field: 'title', sortable: true },
+        { id: 'tracknum', name: '', field: 'tracknum', sortable: true, resizable: false, cssClass: 'tracknum', width: 30 },
+        { id: 'artist', name: 'Artist', field: 'artist', sortable: true },
+        { id: 'album', name: 'Album', field: 'album', sortable: true },
+        { id: 'duration', name: 'Duration', field: 'nice_length', sortable: true, cssClass: 'duration', width: 30 },
+        { id: 'path', name: '', field: 'path' }
+    ];
 
-		if ((currentRow - 1) >= 0) {
-			newRow = currentRow - 1;
-		}
+    var options = {
+        autoHeight: true,
+        editable: false,
+        forceFitColumns: true,
+        enableAutoTooltips: true,
+        enableCellNavigation: false,
+        enableColumnReorder: false,
+        multiSelect: false,
+        rowHeight: 22
+    };
 
-		grid.startPlaying(newRow);
-	}
+    $.getJSON('/songs/index', function(data) {
+        grid = new Slick.Grid("#slickgrid", data, columns, options);
+        grid.setSelectionModel(new Slick.RowSelectionModel());
 
-	function nextSong() {
-		var numberOfRows = grid.getDataLength();
-		var newRow = 0;
-		var currentRow = grid.currentSong; //grid.getSelectedRows()[0];
+        // remove 'path' column
+        grid.setColumns(columns.slice(0, -1));
 
-		if (shuffle) {
-			newRow = randomToN(numberOfRows);
-		}
-		else if ((currentRow + 1) < numberOfRows) {
-			newRow = currentRow + 1;
-		}
+        console.log(grid);
 
-		grid.startPlaying(newRow);
-	}
+        // double-click => play song
+        grid.onClick.subscribe(function (e) {
+            var cell = grid.getCellFromEvent(e);
 
-	function playSong(song, path) {
-		var uri = '/songs/play/?file=' + path;
+            grid.setSelectedRows([cell.row]);
+        });
 
-		audio.attr('src', uri);
-		audio[0].play();
+        grid.onDblClick.subscribe(function (e) {
+            var cell = grid.getCellFromEvent(e);
 
-		playerTrack.text(song);
-	}
+            grid.startPlaying(cell.row);
+
+            e.stopPropagation();
+        });
+
+        grid.onSelectedRowsChanged.subscribe(function (e) {
+            var row = grid.getSelectedRows()[0];
+        });
+
+        grid.currentSong = null;
+
+        grid.startPlaying = function (row) {
+            var song = grid.getDataItem(row);
+            
+            playSong(song.nice_title, song.path);
+
+            // now playing icon
+            var cells = {};
+            cells[row] = { np: 'playing' };
+
+            grid.removeCellCssStyles('currentSong_playing');
+            grid.addCellCssStyles('currentSong_playing', cells);
+
+            grid.currentSong = row;
+            grid.setSelectedRows([row]);
+        };
+    });
 
 
-	// resize the main-area to correct height
-	function resizeMain() {
-		var h = $(window).height() - $('#wrap > header').innerHeight() - $('#player').innerHeight();
-		var w = $(window).width() - $('#sidebar').innerWidth();
-		$('#main').css('height', h);
-		$('#content-wrap').css('width', w);
+    // song playback functions
 
-		if (grid)
-			grid.resizeCanvas();
-	}
+    function prevSong() {
+        var newRow = grid.getDataLength() - 1;
+        var currentRow = grid.currentSong; //grid.getSelectedRows()[0];
 
-	$(window).resize(function () {
-		resizeMain();
-	});
-	$(window).resize();
+        if ((currentRow - 1) >= 0) {
+            newRow = currentRow - 1;
+        }
+
+        grid.startPlaying(newRow);
+    }
+
+    function nextSong() {
+        var numberOfRows = grid.getDataLength();
+        var newRow = 0;
+        var currentRow = grid.currentSong; //grid.getSelectedRows()[0];
+
+        if (shuffle) {
+            newRow = randomToN(numberOfRows);
+        }
+        else if ((currentRow + 1) < numberOfRows) {
+            newRow = currentRow + 1;
+        }
+
+        grid.startPlaying(newRow);
+    }
+
+    function playSong(song, path) {
+        var uri = '/songs/play/?file=' + path;
+
+        audio.attr('src', uri);
+        audio[0].play();
+
+        playerTrack.text(song);
+    }
 
 });
-
-//function to get random number from 1 to n
-function randomToN(maxVal,floatVal)
-{
-   var randVal = Math.random()*maxVal;
-   return typeof floatVal=='undefined'?Math.round(randVal):randVal.toFixed(floatVal);
-}
