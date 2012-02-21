9ea91dce81e2e35b8d55e4251b63c6019edf58fd
ajk <ajk@f2badfa2-83dd-4ef1-9e2d-de321b006519>
2012-02-21 18:19:21 +0000

    Refactor requires
    git-svn-id: https://ajk.tontut.fi/svn/beat-stream/src/trunk@27 f2badfa2-83dd-4ef1-9e2d-de321b006519

--- vendor/assets/javascripts/slick.grid.js
+++ vendor/assets/javascripts/slick.grid.js
@@ -1,1 +1,5 @@
-//= require_tree ./slick.grid
+//= require ./slick.grid/jquery.event.drag-2.0.min
+//= require ./slick.grid/slick.core
+//= require ./slick.grid/slick.grid
+//= require ./slick.grid/slick.dataview
+//= require ./slick.grid/plugins/slick.rowselectionmodel
