975d64c87568f59c7dea30c072d9dbc008847274
Antti-Jussi Kovalainen <dareppi@gmail.com>
2012-08-30 18:04:58 +0300

    Make "production" work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ require 'fileutils'
 
 class ApplicationController < ActionController::Base
     before_filter :authorize
-    before_filter :redirect_to_https
     before_filter :load_sidebar_playlists
-    #protect_from_forgery
 
     protected
 
@@ -13,10 +11,6 @@ class ApplicationController < ActionController::Base
         unless User.find_by_id(session[:user_id])
             redirect_to login_url
         end
-    end
-
-    def redirect_to_https
-        redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
     end
 
     def load_sidebar_playlists
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,4 @@ BeatStream::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  config.serve_static_assets = true
 end
