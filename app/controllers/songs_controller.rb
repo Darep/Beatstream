c04b78301ba6614905e7492237bae94e2d62a8d0
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-06-09 17:57:27 +0300

    Move all the Song related stuff from SongsController to Song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,106 +1,39 @@
-# -*- encoding : utf-8 -*-
-require 'find'
-require 'logger'
-
-MUSIC_PATH = Rails.application.config.music_paths
-
-SONGS_JSON_FILE = Rails.root.join('data/songs.json')
-
 class SongsController < ApplicationController
 
-    # GET /songs
-    def index
-        songs_json = '';
-
-        if params[:refresh]
-            Rails.logger.info 'Forced songs JSON refresh'
-            refresh(songs_json)
-        end
-
-        begin
-            f = File.open(SONGS_JSON_FILE, 'r')
-            Rails.logger.info 'Songs JSON last modified: ' + f.mtime.to_s
-            songs_json = f.read
-        rescue Errno::ENOENT
-            Rails.logger.info 'Songs JSON file not found --> refreshing songs list'
-            refresh(songs_json)
-        end
-
-        render :text => songs_json
+  # GET /songs
+  def index
+    # Backwards compatibility:
+    if params[:refresh]
+      Rails.logger.info 'Forced songs JSON refresh'
+      Song.refresh
     end
 
-    # GET /songs/play
-    def play
-        if params[:file].nil?
-            render :status => :bad_request, :text => "missing required parameter 'file'"
-            return
-        end
+    render :json => Song.all
+  end
 
-        filepath = MUSIC_PATH + params[:file]
-
-        response.content_type = Mime::Type.lookup_by_extension("mp3")
-
-        # Dumb read streaming
-        render :text => File.open(filepath, 'rb') { |f| f.read }
-
-        # True streaming?
-        #send_file filepath, :type => 'audio/mpeg'
+  # GET /songs/play
+  def play
+    if params[:file].nil?
+      render :status => :bad_request, :text => "missing required parameter 'file'" and return
     end
 
-    private
+    # Old, simple file-as-text -rendering:
+    # song = Song.find_by_path(params[:file])
+    # response.content_type = Mime::Type.lookup_by_extension("mp3")
+    # render :text => song.binary_stream
 
-    def refresh(songs_as_json)
-        songs = []
+    filepath = Song.get_path(params[:file])
+    extension = File.extname(filepath)[1..-1]
+    content_type = Mime::Type.lookup_by_extension(extension)
+    content_type = content_type.to_s unless content_type.nil?
+    # render :file => filepath, :content_type => content_type
+    send_file filepath, :type => content_type
+  end
 
-        # TODO: TEST THAT Dir.glob RETURNS THE SAME SET OF SONGS AS Find.find !!111!21!112!!!#!3121
-
-        #Find.find(MUSIC_PATH) do |file|
-        Dir.chdir(MUSIC_PATH)
-        Dir.glob('**/*').each do |file|
-            @invalid = false
-            file = sanitize(file)
-
-            if @invalid
-                Rails.logger.warn "#{file} Contains Invalid UTF-8 byte sequences"
-                # TODO: collect files with invalid UTF-8 and show them to the user
-            end
-
-            # Skip directories
-            if File.directory?(file)
-                next
-            end
-
-            # Skip non-mp3 files
-            if file !~ /.*\.mp3$/i || file =~ /^\./
-#                Rails.logger.info 'Skipping file as not MP3 file: ' + file
-                next
-            end
-
-            begin
-                mp3 = Song.new(file, songs.length)
-                songs.push(mp3)
-            rescue Exception => e
-                Rails.logger.info e
-                Rails.logger.info 'Failed to load MP3: ' + file
-                # TODO: collect the broken mp3s into a separate array
-                # TODO: count the broken mp3s
-            end
-        end
-
-        songs = songs.sort_by { |song| song.to_natural_sort_string }
-
-        songs_as_json = songs.to_json
-        File.open(SONGS_JSON_FILE, 'w') { |f| f.write(songs_as_json) }
-    end
-
-    def sanitize(str)
-        str.each_char.map do |char|
-          begin
-            char if !!char.unpack('U')
-          rescue
-            @invalid = true
-            "\xEF\xBF\xBD" # => "\uFFFD"
-          end
-        end.join
-    end
+  # POST /songs
+  def create
+    # refresh the song list and return new list of songs
+    Song.refresh
+    render :json => Song.all
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,66 +1,151 @@
-require "mp3info"
+require 'mp3info'
 
 class Song
-    attr_reader :id, :filename, :path, :artist, :title, :album, :tracknum, :length
+  MUSIC_PATH = Rails.application.config.music_paths
+  SONGS_JSON_FILE = Rails.root.join('data/songs.json')
 
-    def initialize(path, id)
-        #file = File.new(path)
-        #@size = file.stat.size()
+  attr_reader :id, :filename, :path, :artist, :title, :album, :tracknum, :length
 
-        @filename = File.basename(path)
-        @path = path.gsub(MUSIC_PATH, '')
-        @id = id
+  def initialize(path, id)
+    #file = File.new(path)
+    #@size = file.stat.size()
 
-        @title = @filename
-        @artist = ''
-        @album = ''
-        @tracknum = nil
-        @length = 0
+    @filename = File.basename(path)
+    @path = path.gsub(MUSIC_PATH, '')
+    @id = id
 
-        # ID3 tag info
-        Mp3Info.open(path) do |info|
-            tag = info.tag
-            @title = tag['title'] if (!tag['title'].nil?)
-            @artist = tag['artist'] if (!tag['title'].nil?)
-            @album = tag['album']
-            @tracknum = tag['tracknum']
-            @length = info.length
-        end
+    @title = @filename
+    @artist = ''
+    @album = ''
+    @tracknum = nil
+    @length = 0
 
-        @nice_title = ''
-        @nice_title += (@artist.to_s + ' - ') if !@artist.nil?
-        @nice_title += @title.to_s
-
-        @nice_length = (Time.mktime(0)+@length).strftime("%M:%S")
+    # ID3 tag info
+    Mp3Info.open(path) do |info|
+      tag = info.tag
+      @title = tag['title'] if (!tag['title'].nil?)
+      @artist = tag['artist'] if (!tag['title'].nil?)
+      @album = tag['album']
+      @tracknum = tag['tracknum']
+      @length = info.length
     end
 
-    def to_s
-        @nice_title
+    @nice_title = ''
+    @nice_title += (@artist.to_s + ' - ') if !@artist.nil?
+    @nice_title += @title.to_s
+
+    @nice_length = (Time.mktime(0)+@length).strftime("%M:%S")
+  end
+
+  def binary_stream
+    File.open(Song.get_path(@path), 'rb') { |f| f.read }
+  end
+
+  def to_s
+    @nice_title
+  end
+
+  def to_natural_sort_string
+    str = ""
+    str += artist if !artist.nil?
+    str += ' ' + album if !album.nil?
+
+    # dumb ass way to achieve natural sorting
+    if !tracknum.nil?
+      track = tracknum.to_s
+
+      if tracknum < 100
+        track = "0" + track
+      end
+      if tracknum < 10
+        track = "0" + track
+      end
+
+      str += ' ' + track
     end
 
-    def to_natural_sort_string
-        str = ""
-        str += artist if !artist.nil?
-        str += ' ' + album if !album.nil?
+    if str.empty?
+      str = filename
+    end
 
-        # dumb ass way to achieve natural sorting
-        if !tracknum.nil?
-            track = tracknum.to_s
+    str
+  end
 
-            if tracknum < 100
-                track = "0" + track
-            end
-            if tracknum < 10
-                track = "0" + track
-            end
+  def self.refresh
+    songs = []
 
-            str += ' ' + track
+    Rails.logger.info 'Refreshing songs...'
+
+    Dir.chdir(MUSIC_PATH)
+    Dir.glob('**/*').each do |file|
+      @invalid = false
+      file = Song.sanitize(file)
+
+      if @invalid
+        Rails.logger.warn "#{file} Contains Invalid UTF-8 byte sequences"
+        # TODO: collect files with invalid UTF-8 and show them to the user
+      end
+
+      # Skip directories
+      if File.directory?(file)
+        next
+      end
+
+      # Skip non-mp3 files
+      if file !~ /.*\.mp3$/i || file =~ /^\./
+        # Rails.logger.info 'Skipping file as not MP3 file: ' + file
+        next
+      end
+
+      begin
+        song = Song.new(file, songs.length)
+        songs.push(song)
+      rescue Exception => e
+        Rails.logger.info 'Failed to load MP3: ' + file
+        Rails.logger.info e
+        # TODO: collect the broken mp3s into a separate array?
+      end
+    end
+
+    songs.sort_by! { |song| song.to_natural_sort_string }
+    File.open(SONGS_JSON_FILE, 'w') { |f| f.write(songs.to_json) }
+
+    Rails.logger.info 'Done!'
+  end
+
+  def self.all
+    begin
+      self.read_all
+    rescue Errno::ENOENT
+      Rails.logger.info 'Songs JSON file not found --> refreshing songs list'
+      self.refresh
+      self.read_all
+    end
+  end
+
+  def self.read_all
+    f = File.open(SONGS_JSON_FILE, 'r')
+    Rails.logger.info 'Songs JSON last refreshed on ' + f.mtime.to_s
+    f.read
+  end
+
+  def self.find_by_path(path)
+    Song.new(path, -1)
+  end
+
+  def self.get_path(filepath)
+    MUSIC_PATH + filepath
+  end
+
+  private
+    def self.sanitize(str)
+      str.each_char.map do |char|
+        begin
+          char if !!char.unpack('U')
+        rescue
+          @invalid = true
+          "\xEF\xBF\xBD" # => "\uFFFD"
         end
-
-        if str.empty?
-            str = filename
-        end
-
-        str
+      end.join
     end
 end
