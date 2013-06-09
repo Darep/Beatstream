e523965ec49dd5879eca13ee63c45d420868d1db
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-06-09 18:27:52 +0300

    Clean-up user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 require 'digest/sha2'
 
-
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors.add(attribute, options[:message] || :email) unless
@@ -8,47 +7,45 @@ class EmailValidator < ActiveModel::EachValidator
   end
 end
 
-
 class User < ActiveRecord::Base
-	include ActiveModel::Validations
+  include ActiveModel::Validations
 
-	validates :username, :presence => true, :uniqueness => true, :length => { :minimum => 2 }
-	validates :email, :presence => true, :uniqueness => true, :email => true
-	validates :password, :confirmation => true
+  attr_accessor :password_confirmation
+  attr_reader   :password
 
-	attr_accessor :password_confirmation
-	attr_reader   :password
+  validates :username, :presence => true, :uniqueness => true, :length => { :minimum => 2 }
+  validates :email, :presence => true, :uniqueness => true, :email => true
+  validates :password, :confirmation => true
 
-	validate :password_must_be_present
+  validate :password_must_be_present
 
-	def User.authenticate(username, password)
-		if user = find_by_username(username)
-			if user.hashed_password == encrypt_password(password, user.salt)
-				user
-			end
-		end
-	end
+  def User.authenticate(username, password)
+    if user = find_by_username(username)
+      if user.hashed_password == encrypt_password(password, user.salt)
+        user
+      end
+    end
+  end
 
-	def User.encrypt_password(password, salt)
-		Digest::SHA2.hexdigest(password + 'traktor' + salt)
-	end
+  def User.encrypt_password(password, salt)
+    Digest::SHA2.hexdigest(password + 'traktor' + salt)
+  end
 
-	def password=(password)
-		@password = password
+  def password=(password)
+    @password = password
 
-		if password.present?
-			generate_salt
-			self.hashed_password = self.class.encrypt_password(password, salt)
-		end
-	end
+    if password.present?
+      generate_salt
+      self.hashed_password = self.class.encrypt_password(password, salt)
+    end
+  end
 
-	private
+  private
+    def password_must_be_present
+      errors.add(:password, "Missing password") unless hashed_password.present?
+    end
 
-	def password_must_be_present
-		errors.add(:password, "Missing password") unless hashed_password.present?
-	end
-
-	def generate_salt
-		self.salt = self.object_id.to_s + rand.to_s
-	end
+    def generate_salt
+      self.salt = self.object_id.to_s + rand.to_s
+    end
 end
