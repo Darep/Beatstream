60c0dfdd7c4c612f3cd7c3b95eee1985982e0df5
Antti-Jussi Kovalainen <ajk@ajk.fi>
2014-07-06 19:14:07 +0300

    Always authorized on test env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
     def authorize
+      return true if Rails.env.test?
+
       unless User.find_by_id(session[:user_id])
         redirect_to login_url
       end
