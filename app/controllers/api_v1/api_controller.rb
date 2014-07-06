7ea154bd122edfddb332c839c55e93ac38ef9a42
Antti-Jussi Kovalainen <ajk@ajk.fi>
2014-07-06 19:04:08 +0300

    Add a base controller for API controllers

---
+++ app/controllers/api_v1/api_controller.rb
@@ -0,0 +1,30 @@
+module ApiV1
+  class ApiController < ApplicationController
+    before_filter :set_default_render_format
+
+    rescue_from ActiveRecord::RecordNotFound do |exception|
+      render_404(nil, exception.message)
+    end
+
+    def render_error(message, status = :unprocessable_entity)
+      render :status => status, :json => { :error => message }
+    end
+
+    def render_errors(errors, status = :unprocessable_entity)
+      render :status => status, :json => { :errors => errors }
+    end
+
+    def render_404(class_name = nil, message = nil)
+      class_name ||= params[:controller].classify.demodulize.underscore
+
+      render :json => {
+        :code => 404,
+        :errors => [message || "Couldn't find #{class_name} with id=#{params[:id]}"]
+      }, :status => not_found
+    end
+
+    def set_default_render_format
+      request.format = :json
+    end
+  end
+end
