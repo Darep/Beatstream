2fd26eca695f49948f11fd97c04a7154bb8329a9
Antti-Jussi Kovalainen <ajk@ajk.fi>
2014-07-08 16:20:47 +0300

    Arrange SongTest tests alphabetically

--- test/unit/song_test.rb
+++ test/unit/song_test.rb
@@ -10,9 +10,13 @@ class SongTest < ActiveSupport::TestCase
     Song.refresh
   end
 
-  test 'refresh populates the Songs' do
-    Song.refresh
-    assert_not_empty Song.all_as_json
+  test 'all returns nil when Songs are not populated' do
+    assert_empty Song.all
+  end
+
+  test 'all returns Songs as Hashes when Songs are populated' do
+    populate
+    assert_equal '1sec.mp3', Song.all.first['filename']
   end
 
   test 'all_as_json returns empty JSON array if Songs are not populated' do
@@ -22,15 +26,6 @@ class SongTest < ActiveSupport::TestCase
   test 'all_as_json returns Songs as json when they are populated' do
     populate
     assert_match /"filename": ?"1sec\.mp3"/, Song.all_as_json
-  end
-
-  test 'all returns nil when Songs are not populated' do
-    assert_empty Song.all
-  end
-
-  test 'all returns Songs as Hashes when Songs are populated' do
-    populate
-    assert_equal '1sec.mp3', Song.all.first['filename']
   end
 
   test 'find returns Song with a specific id' do
@@ -44,5 +39,10 @@ class SongTest < ActiveSupport::TestCase
     assert_raise ActiveRecord::RecordNotFound do
       Song.find(1337)
     end
+  end
+
+  test 'refresh populates the Songs' do
+    Song.refresh
+    assert_not_empty Song.all_as_json
   end
 end
