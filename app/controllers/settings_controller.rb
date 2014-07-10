fce3a9d7fdb3b97891550b3976ae3793b7de976a
Antti-Jussi Kovalainen <ajk@ajk.fi>
2014-07-10 22:33:03 +0300

    More DRY in SettingsController

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,41 +1,27 @@
 class SettingsController < ApplicationController
+  layout false
+
   def index
-    render :layout => false
   end
 
   def save
-    if user.update_attributes(params[:user])
-      respond_to do |format|
-        format.html { redirect_to :action => 'index' }
-        format.json { render :nothing => true }
-      end
-    else
-      render :action => 'index', :layout => false
-    end
+    user.update_attributes(params[:user])
+    redirect_to :action => 'index'
   end
 
   def lastfm_callback
-    token = params[:token]
-
-    lastfm_session = Rockstar::Auth.new.session(token)
+    lastfm_session = Rockstar::Auth.new.session(params[:token])
 
     user.update_attributes(
       :lastfm_session_key => lastfm_session.key,
       :lastfm_username => lastfm_session.username
     )
 
-    respond_to do |format|
-      format.html { redirect_to :action => 'index' }
-      format.json { render :nothing => true }
-    end
+    redirect_to :action => 'index'
   end
 
   def lastfm_disconnect
     user.update_attributes(:lastfm_session_key => nil, :lastfm_username => nil)
-
-    respond_to do |format|
-      format.html { redirect_to :action => 'index' }
-      format.json { render :nothing => true }
-    end
+    redirect_to :action => 'index'
   end
 end
