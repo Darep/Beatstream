989c7946e5213287c5253fef026b9f2cad260a5c
Antti-Jussi Kovalainen <ajk@ajk.fi>
2015-02-21 10:16:10 +0200

    Add mockup setting fields to Settings modal

--- app/assets/javascripts/components/settings_modal.js.jsx
+++ app/assets/javascripts/components/settings_modal.js.jsx
@@ -16,17 +16,59 @@ App.SettingsModal = React.createClass({
       <div id="settings-modal" className="modal">
         <div className="modal__content">
           <header className="modal__header">
-            <button type="button" className="close" onClick={this.close}>
+            <button type="button" className="plain-btn close" onClick={this.close}>
               <span aria-hidden="true">&times;</span>
             </button>
             <h1 className="modal__title">Settings</h1>
+            <p className="modal__subtitle">Here are the settings you ordered</p>
           </header>
           <main className="modal__body">
-            <p>Here's the modal you ordered</p>
+
+            <form>
+              <fieldset>
+                <div className="form-row">
+                  <label>Username</label>
+                  <input value="darep" />
+                </div>
+
+                <div className="form-row">
+                  <label>Email</label>
+                  <input value="dareppi@gmail.com" />
+                </div>
+              </fieldset>
+
+              <fieldset>
+                <div className="form-row form-row--inline">
+                  <label>Last.fm</label>
+                  {true &&
+                    <a href="/settings/lastfm_connect" target="_blank" className="btn btn--primary">
+                      Connect Last.fm
+                    </a>
+                  }
+
+                  {false &&
+                    <div>
+                      <p className="lastfm-logged-in">Logged in</p>
+                      <a href="/settings/lastfm_disconnect" className="btn btn--dangerous">
+                        Disconnect Last.fm
+                      </a>
+                    </div>
+                  }
+                </div>
+
+                <div className="form-row form-row--inline">
+                  <label>MP3 Library</label>
+                  <a href="/songs?refresh=1" className="btn btn--primary">
+                    Search for new MP3s
+                  </a>
+                </div>
+              </fieldset>
+            </form>
+
           </main>
           <footer className="modal__footer">
-            <button type="button" className="btn" onClick={this.close}>Close</button>
-            <button type="button" className="btn btn--primary">Save changes</button>
+            <button className="btn btn--big" onClick={this.close}>Close</button>
+            <button className="btn btn--big btn--primary">Save changes</button>
           </footer>
         </div>
       </div>
