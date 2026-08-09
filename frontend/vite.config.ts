411661c7da6c480a755e11e1fcbe4643b3713dab
AJ Kovalainen <ajk@ajk.fi>
2026-08-09 23:08:31 +0300

    fix(deps): update frontend and backend dependencies (#82)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: { tsconfigPaths: true },
 });
