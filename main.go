8bdd8151d48ccacee8a6d3099e71a2daa4c6dab2
AJ Kovalainen <ajk@ajk.fi>
2026-08-15 23:05:06 +0000

    feat: add DATA_PATH

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/Darep/Beatstream/logger"
 	"github.com/joho/godotenv"
 )
 
 var MusicPath string
+var DataPath = "."
 var version = "dev"
 var revision = "unknown"
+
+func dataFilePath(name string) string {
+	return filepath.Join(DataPath, name)
+}
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "--version" {
@@ -23,6 +29,12 @@ func main() {
 	MusicPath = os.Getenv("MUSIC_PATH")
 	if MusicPath == "" {
 		logger.Log.Fatal("MUSIC_PATH environment variable not set")
+	}
+	if path := os.Getenv("DATA_PATH"); path != "" {
+		DataPath = path
+	}
+	if err := os.MkdirAll(DataPath, 0o700); err != nil {
+		logger.Log.Fatalf("Failed to create data directory: %v", err)
 	}
 
 	err := loadUsers()
